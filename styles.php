172b5202b5215485d61a2f40e3f14f63601921bc
Albert Gasset <albert.gasset@gmail.com>
2011-09-07 16:41:04 +0200

    cache de 24 hores

--- styles.php
+++ styles.php
@@ -7,7 +7,7 @@
 /// THERE SHOULD BE NO NEED TO MODIFY THIS FILE!!  USE CONFIG.PHP INSTEAD.
 
 
-    $lifetime  = (30 * 24 * 3600) + 1;                       // Seconds to cache this stylesheet
+    $lifetime = 24 * 3600;                              // Seconds to cache this stylesheet
     $nomoodlecookie = true;                             // Cookies prevent caching, so don't use them
     require_once("../../config.php");                   // Load up the Moodle libraries
     $themename = basename(dirname(__FILE__));           // Name of the folder we are in
